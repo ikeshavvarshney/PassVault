f2d8ada03e5f7fb297de764a63015045e6b920c8
Keshav Varshney <ikeshavvarshney@gmail.com>
2025-10-24 19:26:22 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,22 @@ function App() {
     };
     setPasswords(newPasswords)
     localStorage.setItem('passwords', JSON.stringify(newPasswords))
-    alert('Password saved!')
+    toast.success(`Password saved successfully!`, {
+      position: "top-right",
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+      transition: Bounce,
+      style: {
+        background: "#10B981",
+        color: "#fff",
+        fontWeight: "500",
+        borderRadius: "10px",
+      },
+    });
     setform({ url: '', username: '', password: '' })
   }
   const handleChange = (e) => {
@@ -53,7 +68,7 @@ function App() {
     });
   }
   const editPassword = (url, username) => {
-    setform({url : url, username : username , password : passwords[url][username]})
+    setform({ url: url, username: username, password: passwords[url][username] })
   }
   const deletePassword = (url, username) => {
     if (confirm('Are you sure you want to delete this password?')) {
@@ -96,7 +111,7 @@ function App() {
       />
       <div className="App">
         <Navbar></Navbar>
-        <main className='flex flex-col items-center w-auto min-h-screen bg-green-200 bg-opacity-50 mx-35 my-10 rounded-2xl p-8 gap-5'>
+        <main className='flex flex-col items-center w-auto min-h-screen bg-green-200 bg-opacity-50 md:mx-35 md:my-10 rounded-2xl px-2 md:px-8 py-8 gap-5'>
 
           <div className='flex flex-col items-center'>
             <div className='flex justify-center'>
@@ -113,12 +128,12 @@ function App() {
           </div>
 
           <div className='flex flex-col gap-2 items-center'>
-            <div className='w-full'>
-              <input type="text" name='url' value={form.url} onChange={handleChange} placeholder='Enter website URL...' className="px-5 py-2 border-2 border-gray-700 rounded-full w-3xl text-black hover:ring-2 hover:ring-green-400 focus:outline-none focus:ring-2 focus:ring-green-400 transition-all" />
+            <div className='w-fit flex justify-center items-center'>
+              <input type="text" name='url' value={form.url} onChange={handleChange} placeholder='Enter website URL...' className="px-5 py-2 border-2 border-gray-700 rounded-full w-2xs sm:w-sm md:w-3xl text-black hover:ring-2 hover:ring-green-400 focus:outline-none focus:ring-2 focus:ring-green-400 transition-all" />
             </div>
-            <div className='flex gap-1 relative'>
-              <input type="text" name='username' value={form.username} onChange={handleChange} placeholder='Enter username...' className="px-5 py-2 border-2 border-gray-700 rounded-full w-sm text-black hover:ring-2 hover:ring-green-400 focus:outline-none focus:ring-2 focus:ring-green-400 transition-all" />
-              <input type={showPassword ? "text" : "password"} name='password' value={form.password} onChange={handleChange} placeholder='Enter password...' className="pl-5 pr-10 py-2 border-2 border-gray-700 rounded-full w-sm text-black hover:ring-2 hover:ring-green-400 focus:outline-none focus:ring-2 focus:ring-green-400 transition-all" />
+            <div className='flex flex-col md:flex-row gap-1 relative'>
+              <input type="text" name='username' value={form.username} onChange={handleChange} placeholder='Enter username...' className="px-5 py-2 border-2 border-gray-700 rounded-full w-2xs sm:w-sm text-black hover:ring-2 hover:ring-green-400 focus:outline-none focus:ring-2 focus:ring-green-400 transition-all" />
+              <input type={showPassword ? "text" : "password"} name='password' value={form.password} onChange={handleChange} placeholder='Enter password...' className="pl-5 pr-10 py-2 border-2 border-gray-700 rounded-full w-2xs sm:w-sm text-black hover:ring-2 hover:ring-green-400 focus:outline-none focus:ring-2 focus:ring-green-400 transition-all" />
               <button onClick={() => setShowPassword(!showPassword)} className="absolute right-3 top-2.5">
                 <lord-icon
                   src="https://cdn.lordicon.com/dicvhxpz.json"
@@ -151,9 +166,9 @@ function App() {
             </button>
           </div>
 
-          <div className="passwords flex flex-col items-center gap-2 py-2">
-            <div className="flex justify-between w-full items-center">
-              <span className='text-2xl font-semibold'>Your Passwords</span>
+          <div className="passwords flex flex-col items-center gap-2 py-2 w-full">
+            <div className="flex justify-between w-full md:w-2/3 items-center">
+              <span className='text-md md:text-2xl font-semibold'>Your Passwords</span>
               <div className='flex items-center justify-center gap-1'>
                 <button id='showAllPassword' onClick={() => setShowAllPassword(!showAllPassword)} className="flex items-center justify-center">
                   <lord-icon
@@ -163,73 +178,78 @@ function App() {
                     style={{ width: "25px", height: "25px" }}>
                   </lord-icon>
                 </button>
-                <label htmlFor="showAllPassword" className='w-[170px]'>{showAllPassword ? "Hide All Passwords" : "Show All Passwords"}</label>
+                <label htmlFor="showAllPassword" className='w-[140px]'>{showAllPassword ? "Hide All Passwords" : "Show All Passwords"}</label>
               </div>
             </div>
-            {Object.keys(passwords).length === 0 && (
-              <h2 className="text-xl font-semibold text-gray-600 italic">No passwords saved yet.</h2>
-            )}
-            {Object.keys(passwords).map((url) => (
-              <div key={url} className="w-[50vw] bg-green-100 border border-green-300 rounded-2xl p-3 shadow-sm hover:shadow-lg hover:border-green-500 transition-all duration-300">
-                <div className="flex justify-between items-center">
-                  <h3 className="flex items-center text-lg font-bold text-green-800 w-[45vw] relative">
-                    <span className="font-semibold text-gray-800 w-5/12">Website: {url}</span>
-                    <button onClick={() => copy('URL', url)} className='inline-flex justify-center items-center'>
-                      <lord-icon
-                        src="https://cdn.lordicon.com/iykgtsbt.json"
-                        trigger="hover"
-                        style={{ width: "18px", height: "18px" }}>
-                      </lord-icon>
-                    </button>
-                  </h3>
-                </div>
 
-                {Object.keys(passwords[url]).map((username) => (
-                  <div className="flex justify-between" key={username}>
-                    <p className="text-md text-gray-700 flex justify-between items-center w-5/12">
-                      <span className="font-medium text-green-900">Username: {username}</span>
-                      <button onClick={() => copy('Username', username)} className='inline-flex justify-center items-center'>
+            <div className="flex flex-col gap-2 items-center w-full">
+              {Object.keys(passwords).length === 0 && (
+                <h2 className="text-xl font-semibold text-gray-600 italic">No passwords saved yet.</h2>
+              )}
+              {Object.keys(passwords).map((url) => (
+                <div key={url} className="w-[95vw] md:w-[50vw] bg-green-100 border border-green-300 rounded-2xl p-3 shadow-sm hover:shadow-lg hover:border-green-500 transition-all duration-300">
+                  <div className="flex justify-between items-center w-full">
+                    <h3 className="flex items-center text-md md:text-lg font-bold text-green-800 w-full md:w-[41vw] relative">
+                      <span className="font-semibold text-gray-800 w-full md:w-5/12">Website: {url}</span>
+                      <button onClick={() => copy('URL', url)} className='inline-flex justify-center items-center'>
                         <lord-icon
                           src="https://cdn.lordicon.com/iykgtsbt.json"
                           trigger="hover"
                           style={{ width: "18px", height: "18px" }}>
                         </lord-icon>
                       </button>
-                    </p>
-
-                    <div className=" w-5/14 flex justify-between items-center">
-                      <p className="text-md text-gray-700">Password:{" "}
-                        <span className="font-mono text-green-900 bg-green-200 rounded px-2 py-[2px]">{showAllPassword ? passwords[url][username] : "********"}</span>
-                      </p>
-                      <button onClick={() => copy('Password', passwords[url][username])} className='inline-flex justify-center items-center'>
-                        <lord-icon
-                          src="https://cdn.lordicon.com/iykgtsbt.json"
-                          trigger="hover"
-                          style={{ width: "18px", height: "18px" }}>
-                        </lord-icon>
-                      </button>
-                    </div>
-
-                    <div className="flex gap-1">
-                      <button onClick={() => editPassword(url, username)} className='inline-flex justify-center items-center'>
-                        <lord-icon
-                          src="https://cdn.lordicon.com/gwlusjdu.json"
-                          trigger="hover"
-                          style={{ width: "18px", height: "18px" }}>
-                        </lord-icon>
-                      </button>
-                      <button onClick={() => deletePassword(url, username)} className='inline-flex justify-center items-center'>
-                        <lord-icon
-                          src="https://cdn.lordicon.com/oqeixref.json"
-                          trigger="hover"
-                          style={{ width: "18px", height: "18px" }}>
-                        </lord-icon>
-                      </button>
-                    </div>
+                    </h3>
                   </div>
-                ))}
-              </div>
-            ))}
+
+                  {Object.keys(passwords[url]).map((username) => (
+                    <div className="flex justify-between gap-2" key={username}>
+                      <div className="flex flex-col md:flex-row w-full justify-between">
+                        <div className="text-md text-gray-700 flex justify-between items-center w-full md:w-5/12">
+                          <span className="font-medium text-green-900">Username: {username}</span>
+                          <button onClick={() => copy('Username', username)} className='inline-flex justify-center items-center'>
+                            <lord-icon
+                              src="https://cdn.lordicon.com/iykgtsbt.json"
+                              trigger="hover"
+                              style={{ width: "18px", height: "18px" }}>
+                            </lord-icon>
+                          </button>
+                        </div>
+
+                        <div className="w-full md:w-5/14 flex justify-between items-center">
+                          <p className="text-md text-gray-700">Password:{" "}
+                            <span className="font-mono text-green-900 bg-green-200 rounded px-2 py-[2px]">{showAllPassword ? passwords[url][username] : "********"}</span>
+                          </p>
+                          <button onClick={() => copy('Password', passwords[url][username])} className='inline-flex justify-center items-center'>
+                            <lord-icon
+                              src="https://cdn.lordicon.com/iykgtsbt.json"
+                              trigger="hover"
+                              style={{ width: "18px", height: "18px" }}>
+                            </lord-icon>
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className="flex gap-1">
+                        <button onClick={() => editPassword(url, username)} className='inline-flex justify-center items-center'>
+                          <lord-icon
+                            src="https://cdn.lordicon.com/gwlusjdu.json"
+                            trigger="hover"
+                            style={{ width: "18px", height: "18px" }}>
+                          </lord-icon>
+                        </button>
+                        <button onClick={() => deletePassword(url, username)} className='inline-flex justify-center items-center'>
+                          <lord-icon
+                            src="https://cdn.lordicon.com/oqeixref.json"
+                            trigger="hover"
+                            style={{ width: "18px", height: "18px" }}>
+                          </lord-icon>
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
           </div>
 
         </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,8 @@
         <img src="/logo.svg" alt="PassVault" width={150} />
       </div>
 
-      <div className="search relative">
-        <button className="cursor-pointer absolute left-3 top-2.5 text-gray-500">
-          <lord-icon
-            src="https://cdn.lordicon.com/xaekjsls.json"
-            trigger="hover"
-            state="morph-select"
-            colors="primary:#ffffff"
-            style={{ width: "24px", height: "24px" }}>
-          </lord-icon>
-        </button>
-        <input type="text" placeholder="Search..." className="pl-10 pr-4 py-2 border-2 border-gray-700 rounded-full w-md text-[#ffffff] hover:ring-2 hover:ring-green-400 focus:outline-none focus:ring-2 focus:ring-green-400 transition-all" />
-      </div>
-
       <div className="tabs flex gap-2">
         <a href='/' className="tab-button px-4 py-2 rounded-lg hover:bg-green-600 active:bg-green-600 transition-all">Home</a>
-        <a href="https://github.com/ikeshavvarshney" className="tab-button px-4 py-2 rounded-lg hover:bg-green-600 active:bg-green-600 transition-all">Contact</a>
         <a href="https://github.com/ikeshavvarshney/PassVault" className="tab-button px-4 py-2 rounded-lg hover:bg-green-600 active:bg-green-600 transition-all">GitHub</a>
       </div>
 
